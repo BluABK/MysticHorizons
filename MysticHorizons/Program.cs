76330e25e159eccbd976eb8b7a8c77e23b5ac0da
BluABK <bluabk@bluabk.net>
2021-09-06 14:06:38 +0200

    Add project files.

---
+++ MysticHorizons/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MysticHorizons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
