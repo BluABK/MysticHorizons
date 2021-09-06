47d986163f738594e1f37951c9d12f8293cd79d1
BluABK <bluabk@bluabk.net>
2021-09-06 14:36:46 +0200

    Init project

--- MysticHorizons/Program.cs
+++ MysticHorizons/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MysticHorizonsLib;
 
 namespace MysticHorizons
 {
@@ -6,7 +7,14 @@ namespace MysticHorizons
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GameCharacter player = new("Hero", health: 100);
+
+            // Simulate a hit to HP:
+            player.Health.Value = 25;
+
+            player.AboutMe();
+
+            Console.WriteLine("The boss you are looking for is in another dungeon..."); // FIXME: Remove when any form of gameplay is actually implemented.
         }
     }
 }
